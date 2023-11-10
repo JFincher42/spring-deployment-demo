e06e1557fd5d85b60d31d8712ea0b1f28b009f48
Jon Fincher <jfincher42@gmail.com>
2023-11-10 11:04:43 -0600

    Setting up for WAR deployment

--- src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
+++ src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringDeploymentDemoApplication {
+public class SpringDeploymentDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDeploymentDemoApplication.class, args);
